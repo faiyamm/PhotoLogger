c0fa13823e428841bac97cd610e3aa6746076196
faiyamm <sofia.almaraz@uabc.edu.mx>
2026-03-11 21:42:00 -0700

    Initial Commit

---
+++ PhotoLogger/PhotoLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoLoggerApp.swift
+//  PhotoLogger
+//
+//  Created by Fai on 11/03/26.
+//
+
+import SwiftUI
+
+@main
+struct PhotoLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
